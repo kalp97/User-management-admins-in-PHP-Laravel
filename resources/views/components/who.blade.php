e6278cdfc96b26b1c9f483765b5c126e0c10c92f
kalp97 <kdd32@njit.edu>
2018-12-14 20:19:10 -0500

    Create who.blade file to prompt the LogIn-LogOut status to Admin and User.

--- resources/views/components/who.blade.php
+++ resources/views/components/who.blade.php
@@ -1,19 +1,20 @@
 @if (Auth::guard('web')->check())
-    <p class="text-success">
+    <p class="text-success" style="text-align:center">
+
         You are Logged In as a <strong>USER</strong>
     </p>
 @else
-    <p class="text-danger">
+    <p class="text-danger" style="text-align:center">
         You are Logged Out as a <strong>USER</strong>
     </p>
 @endif
 
 @if (Auth::guard('admin')->check())
-    <p class="text-success">
+    <p class="text-success" style="text-align:center">
         You are Logged In as a <strong>ADMIN</strong>
     </p>
 @else
-    <p class="text-danger">
+    <p class="text-danger" style="text-align:center">
         You are Logged Out as a <strong>ADMIN</strong>
     </p>
 @endif
