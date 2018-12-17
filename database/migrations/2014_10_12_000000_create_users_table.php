51c4c860a61bebf197dd5ca0ca318b43cfab4ee8
kalp97 <kdd32@njit.edu>
2018-12-16 20:35:21 -0500

    Edit: edited users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->boolean('is_active')->default(false);
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+        //
     }
 
     /**
