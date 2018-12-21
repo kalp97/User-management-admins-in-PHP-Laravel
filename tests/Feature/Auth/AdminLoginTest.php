a38ed80c4d2418aa86375ef82e21a34d3dc97026
kalp97 <kdd32@njit.edu>
2018-12-21 15:13:07 -0500

    Test to see if Admin can see his Login Page.

---
+++ tests/Feature/Auth/AdminLoginTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Feature\Auth;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class AdminLoginTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_admin_can_view_a_login_form()
+    {
+        $response = $this->get('/admin/login');
+        $response->assertSuccessful();
+    }
+}
